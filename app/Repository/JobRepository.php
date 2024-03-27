7e58c98db0f6416097120bfc350f6e87ee87d38b
Emmanuel Abraham <sundayemmanuelabraham@gmail.com>
2024-03-27 13:16:00 +0100

    added job role repository

--- app/Repository/JobRepository.php
+++ app/Repository/JobRepository.php
@@ -4,3 +4,50 @@ namespace App\Repository;
 
 use App\Models\JobRole;
 use App\Services\JobRoleService;
+
+class JobRoleRepository 
+{
+  /**
+   * Get all job roles
+   * @return Array | Collection
+   */
+  public function findAll(): Array | Collection
+  {
+    return JobRole::all();
+  }
+
+  /**
+   * Create a new job role
+   * @param array JobRole $data | null
+   */
+  public function create(array $data): JobRole | null
+  {
+    return JobRole::create($data);
+  }
+
+  /**
+   * Update a job role
+   * @param array $data
+   * @param int $id
+   * @return JobRole | null
+   */
+  public function findOneAndUpdate(array $data, int $id): JobRole | null
+  {
+    return JobRole::where('id', $id)->update($data);
+  }
+
+  /**
+   * Delete a job role
+   * @param int $id
+   * @return bool
+   */
+  public function delete(int $id): bool
+  {
+    try {
+      JobRole::where('id', $id)->delete();
+      return true;
+    } catch (\Exception $e) {
+      return false;
+    }
+  }
+}
